b35bdb9b02bedd1f9c2c4dcb4a15de197e7a735d
ubangura <unisa.bangura@outlook.com>
2023-09-24 13:19:02 -0400

    feat: create account

--- apply.js
+++ apply.js
@@ -1,16 +1,41 @@
-import { } from './information.js';
+import { email, password } from './information.js';
 import puppeteer from "puppeteer";
 
-(async () => {
+apply();
+
+async function apply() {
+    var page = await getPage();
+    await page.goto(
+        "https://comcast.wd5.myworkdayjobs.com/en-US/Comcast_Careers/job/Virtual/Software-Engineer-Intern--Data-ML-and-Algro_R371953"
+    );
+    await createAccount(page);
+}
+
+async function getPage() {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: false,
     });
     const page = await browser.newPage();
+    return page;
+}
 
-    await page.goto(
-        "url"
-    );
+async function createAccount(page) {
+    await page.locator('a[data-automation-id="adventureButton"]').click();
 
-    await browser.close();
-})();
+    await page.locator('a[data-automation-id="applyManually"]').click();
+
+    await page.locator('button[data-automation-id="createAccountLink"]').click();
+
+    await page.locator('input[data-automation-id="email"]').fill(email);
+
+    await page.locator('input[data-automation-id="password"]').fill(password);
+    await page.locator('input[data-automation-id="verifyPassword"]').fill(password);
+
+    const createAccountCheckbox = await page.$('input[data-automation-id="createAccountCheckbox"]');
+    if (createAccountCheckbox != null) {
+        await createAccountCheckbox.click();
+    }
+
+    await page.locator('button[data-automation-id="createAccountSubmitButton"]').click();
+}
--- information.js
+++ information.js
@@ -1,1 +1,4 @@
-export { };
+const email = "email@email.com";
+const password = "To@void3rrors-->"; // Most applications require 8 characters (some require 16), a capital letter, a numeric character, and a special character
+
+export { email, password };
