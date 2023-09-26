2ac7de18c6058791d5d6a7280764e657feeffb01
ubangura <unisa.bangura@outlook.com>
2023-09-26 16:56:51 -0400

    feat: fill out basic information

--- apply.js
+++ apply.js
@@ -1,4 +1,4 @@
-import { email, password } from './information.js';
+import { email, password, firstName, lastName, suffix, street, city, state, postalCode, phoneType, phoneNumber } from './information.js';
 import puppeteer from "puppeteer";
 
 apply();
@@ -14,6 +14,8 @@ async function apply() {
     if (await selectorExists(page, 'div[data-automation-id="errorMessage"]')) {
         await signIn(page);
     }
+
+    await fillBasicInfo(page);
 }
 
 async function getPage() {
@@ -51,7 +53,7 @@ async function createAccount(page) {
 
 async function selectorExists(page, selector) {
     try {
-        await page.waitForSelector(selector, { timeout: 800 });
+        await page.waitForSelector(selector, { timeout: 1000 });
     } catch (error) {
         return false;
     }
@@ -68,4 +70,33 @@ async function signIn(page) {
     await page.locator('input[data-automation-id="password"]').fill(password);
 
     await page.locator('button[data-automation-id="signInSubmitButton"]').click({ delay: 400 });
+}
+
+async function fillBasicInfo(page) {
+    console.log("Filling basic info");
+
+    await page.locator('div[data-automation-id="previousWorker"] input[id="2"]').click();
+
+    await page.locator('input[data-automation-id="legalNameSection_firstName"]').fill(firstName);
+    await page.locator('input[data-automation-id="legalNameSection_lastName"]').fill(lastName);
+    const suffixDropdown = 'button[data-automation-id="legalNameSection_social"]';
+    if (await selectorExists(page, suffixDropdown)) {
+        await page.click(suffixDropdown);
+        await page.keyboard.type(suffix);
+        await page.keyboard.press('Enter');
+    }
+
+    await page.locator('input[data-automation-id="addressSection_addressLine1"]').fill(street);
+    await page.locator('input[data-automation-id="addressSection_city"]').fill(city);
+    await page.locator('button[data-automation-id="addressSection_countryRegion"]').click();
+    await page.keyboard.type(state, { delay: 100 });
+    await page.keyboard.press('Enter');
+    await page.locator('input[data-automation-id="addressSection_postalCode"]').fill(postalCode);
+
+    await page.locator('button[data-automation-id="phone-device-type"]').click();
+    await page.keyboard.type(phoneType, {delay: 100});
+    await page.keyboard.press('Enter');
+    await page.locator('input[data-automation-id="phone-number"]').fill(phoneNumber);
+
+    await page.locator('button[data-automation-id="bottom-navigation-next-button"]').click();
 }
--- information.js
+++ information.js
@@ -1,4 +1,13 @@
 const email = "email@email.com";
 const password = "To@void3rrors-->"; // Most applications require 8 characters (some require 16), a capital letter, a numeric character, and a special character
+const firstName = "Hire";
+const lastName = "Me";
+const suffix = "Jr";
+const street = "1600 Amphitheatre Parkway";
+const city = "Mountain View"
+const state = "CA";
+const postalCode = "94043";
+const phoneType = "Mobile";
+const phoneNumber = "(650)-253-0000";
 
-export { email, password };
+export { email, password, firstName, lastName, suffix, street, city, state, postalCode, phoneType, phoneNumber };
